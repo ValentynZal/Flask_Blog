73b78a9972e9775d35b63d9b70c1a2075060b6eb
ValentynZal <valentin11091994@gmail.com>
2019-08-30 16:04:38 +0300

    user authentication

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4090b112f4ca6b3972ca1deab52b71fd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # specify db location
 
 db = SQLAlchemy(app) # create db instance
+bcrypt = Bcrypt(app) # for hashing password
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # for redirect when trying to access to account page as logout
+login_manager.login_message_category = 'info' # for alert message |^
 
 from blog import routes
